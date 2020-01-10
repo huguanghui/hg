0f83af43b4c966f78a723b88ae184759bad27b8e
huguanghui <522146829@qq.com>
2020-01-10 11:40:51 +0800

    refresh

--- iniopt/iniopt.cc
+++ iniopt/iniopt.cc
@@ -4,7 +4,7 @@
 #include <unistd.h>
 
 #include "base/def.h"
-#include "base/str.h"
+#include "base/str.h" 
 #include "base/fs.h"
 #include "base/flag.h"
 #include "base/log.h"
@@ -15,6 +15,7 @@ static int handler_get(std::vector<fastring> v);
 static int handler_set(std::vector<fastring> v);
 static int handler_check(std::vector<fastring> v);
 static int handler_merge(std::vector<fastring> v);
+static int handler_dbg(std::vector<fastring> v);
 
 typedef int (*opt_callback)(std::vector<fastring> v);
 
@@ -23,7 +24,6 @@ struct OptHandler
     char opt[8];
     opt_callback handler;
 };
-
 static void usage()
 {
     COUT << "usage:\n"
@@ -70,6 +70,8 @@ int main(int argc, char** argv)
         {"get",     handler_get},
         {"set",     handler_set},
         {"check",   handler_check},
+        {"merge",   handler_merge},
+        //{"dbg",     handler_dbg},
     };
 
     for (size_t i = 0; i < sizeof(optmap)/sizeof(optmap[0]); ++i)
@@ -79,6 +81,8 @@ int main(int argc, char** argv)
             optmap[i].handler(v);
         }
     }
+
+    
 
     return status ;
 }
@@ -156,7 +160,7 @@ int handler_check(std::vector<fastring> v)
     return 0;
 }
 
-nt handler_merge(std::vector<fastring> v)
+int handler_merge(std::vector<fastring> v)
 {
     fastring file(v[0]);
     fastring subfile(v[2]);
@@ -166,6 +170,71 @@ nt handler_merge(std::vector<fastring> v)
     {
         return 0;
     }
+
+    dictionary *subdict = iniparser_load(subfile.c_str());
+    if (NULL == subdict)
+    {
+        return 0;
+    }
+    const char *session = NULL;
+    int nsec = 0;
+    int nkey = 0;
+    const char **arry_key = NULL;
+    const char **str_rc = NULL;
+    int int_rc = 0;
+    const char *str_value = NULL;
+
+    nsec = iniparser_getnsec(subdict);
+    for (int i = 0; i < nsec; ++i)
+    {
+        session = iniparser_getsecname(subdict, i);
+        if (session != NULL)
+        {
+            //COUT << "session: " << session << "\n";
+            nkey = iniparser_getsecnkeys(subdict, session);
+            arry_key = (const char**)malloc(sizeof(char*) * nkey);
+            str_rc = iniparser_getseckeys(subdict, session, arry_key);  
+            if (str_rc == arry_key)
+            {
+                for (int k = 0; k < nkey; ++k)
+                {
+                    //COUT << "key: " << arry_key[k] << "\n";
+                    int_rc = iniparser_find_entry(dict, arry_key[k]);
+                    if (1 == int_rc)
+                    {
+                       str_value = iniparser_getstring(subdict, arry_key[k], "null-1"); 
+                       //COUT << "rc[" << int_rc << "], key[" << arry_key[k] << "] value[" << str_value << "]\n";
+                       iniparser_set(dict, arry_key[k], str_value);
+                    }
+                    else
+                    {
+
+                       //COUT << "rc[" << int_rc << "], key[" << arry_key[k] << "]\n";
+                    }
+                }
+            }
+        }
+    }
+
+    iniparser_freedict(subdict);
+
+    iniparser_save(dict, file.c_str());
+    iniparser_freedict(dict);
+
+    return 0;
+}
+
+int handler_dbg(std::vector<fastring> v)
+{
+    
+    fastring file(v[0]);
+
+    dictionary *dict = iniparser_load(file.c_str());
+    if (NULL == dict)
+    {
+        return 0;
+    }
+    iniparser_save(dict, file.c_str());
     iniparser_freedict(dict);
 
     return 0;

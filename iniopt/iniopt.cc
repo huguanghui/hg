e0f5a31787aa894b391e6a1c4bac6ec83dea9dbd
huguanghui <522146829@qq.com>
2020-01-15 15:16:02 +0800

    1.新增bin包解压和合并程序

--- iniopt/iniopt.cc
+++ iniopt/iniopt.cc
@@ -82,8 +82,6 @@ int main(int argc, char** argv)
         }
     }
 
-    
-
     return status ;
 }
 
---
+++ zw_decpack/xmake.lua
@@ -0,0 +1,19 @@
+target("zw_decpack")
+    set_kind("binary")
+    add_cxxflags("-std=c++11")
+    set_optimize("faster")
+    set_warnings("all")
+    set_targetdir("../build")
+    add_includedirs("..")
+    add_includedirs("../include")
+    add_linkdirs("../lib")
+    add_links("base")
+    add_files("*.cc")
+
+    add_cxflags("-g3")
+    add_syslinks("pthread", "dl")
+
+    after_build(function ()
+        os.rm("build")
+    end)
+    

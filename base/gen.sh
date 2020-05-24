013367df2411f64b2c2453a64fb1598efdb64c1f
huguanghui <522146829@qq.com>
2020-05-24 21:25:02 +0800

    添加交叉编译参数

--- base/gen.sh
+++ base/gen.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-xmake f -p linux --sdk=/opt/hisi-linux/x86-arm/arm-himix200-linux -a arm -m debug
+xmake f -p linux --cross=arm-himix200-linux- --sdk=/opt/hisi-linux/x86-arm/arm-himix200-linux -a arm -m debug
+#xmake f -p linux --sdk=/opt/hisi-linux/x86-arm/arm-himix200-linux -a arm -m debug
 #xmake f -p linux --sdk=/opt/hisi-linux/x86-arm/arm-hisiv500-linux -m debug
